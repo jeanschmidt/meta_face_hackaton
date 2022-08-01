224e86e0f4b52900591753df5bf04a89ec726c54
Jean Schmidt <contato@jschmidt.me>
2022-08-01 13:14:54 +0200

    ...more improvements...

--- detect_face/__main__.py
+++ detect_face/__main__.py
@@ -1,21 +1,28 @@
+from collections import defaultdict
+from facenet_pytorch import MTCNN
 from scipy import spatial
 from skimage import io
+from tqdm import tqdm
 import argparse
 import cv2
 import itertools
+import math
 import numpy as np
 import sys
 import torch
+
 from .model import APN_Model
-from collections import defaultdict
+
 
 MODEL_INPUT_SHAPE = (128, 128)
 
 
 def get_args():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-v", "--video", required=True,
+    ap.add_argument("-v", "--video", required=False,
     	help="video to detect faces")
+    ap.add_argument("-i", "--image", required=False,
+    	help="image to detect faces")
     ap.add_argument("-b", "--badges", nargs='+', required=True,
     	help="badges as references")
     ap.add_argument('-d', '--debug', default=False, action='store_true',
@@ -30,46 +37,45 @@ def img_2_vec(model, img):
 
 
 def get_best_face_image(face_cascade, img_path):
-    img = cv2.imread(img_path)
+    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
 
-    biggest_face = None
-    quality = 9
-    while biggest_face is None:
-        faces = face_cascade.detectMultiScale(img, 1.1, quality)
+    faces, scores = face_cascade.detect(img)
 
-        for face in faces:
-            if biggest_face is None or biggest_face[2] > face[2]:
-                biggest_face = face
+    if not faces.shape[0]:
+        raise Exception(f"could not detect a face in image {img_path}")
 
-        if quality == 1:
-            break
-        quality -= 1
-
-    if biggest_face is None:
-        raise Exception(f"[INFO] Could not detect a face in {img_path}")
-
-    (x, y, w, h) = biggest_face
-
-    cropped = img[y:y+h, x:x+w]
-    return cv2.resize(cropped, MODEL_INPUT_SHAPE, interpolation=cv2.INTER_LANCZOS4)
+    x, y, xx, yy = [int(n) for n in faces[0]]
+    s = max(xx - x, yy - y)
+    xx = x + s
+    yy = y + s
+    cropped = img[y:yy, x:xx]
+    resized = cv2.resize(cropped, MODEL_INPUT_SHAPE, interpolation=cv2.INTER_LANCZOS4)
+    # cv2.imshow('frame_name', resized)
+    # cv2.waitKey()
+    return resized
 
 
 def get_all_faces_image(face_cascade, model, img):
-    faces = face_cascade.detectMultiScale(img, 1.1, 3)
+    faces, scores = face_cascade.detect(img)
 
     ret = []
     for face in faces:
-        (x, y, w, h) = face
+        x, y, xx, yy = int(math.floor(face[0])), int(math.floor(face[1])), int(math.ceil(face[2])), int(math.ceil(face[3])),
+        [int(n) for n in face]
+        s = max(xx - x, yy - y)
+        xx = x + s
+        yy = y + s
 
-        cropped = img[y:y+h, x:x+w]
-        # cv2.imshow('frame_name', cropped)
+        cropped = img[y:yy, x:xx]
+        if cropped.size < 1:
+            continue
+
+        resized = cv2.resize(cropped, MODEL_INPUT_SHAPE, interpolation=cv2.INTER_LANCZOS4)
+        # cv2.imshow('frame_name', resized)
         # cv2.waitKey()
         ret.append((
             face,
-            img_2_vec(
-                model,
-                cv2.resize(cropped, MODEL_INPUT_SHAPE, interpolation=cv2.INTER_LANCZOS4)
-            ),
+            img_2_vec(model, resized),
         ))
 
     return ret
@@ -96,23 +102,39 @@ def reorder_closest_points(last, atual):
             yield last_frame_face
 
 def process_one_frame(face_cascade, model, frame, args, badges_faces):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_faces = get_all_faces_image(face_cascade, model, frame)
     faces_scores = []
 
     for frame_face_idx in range(len(frame_faces)):
         face_coord, face_embedding = frame_faces[frame_face_idx]
 
+        temp_faces = []
         for badge_idx in range(len(args["badges"])):
             dist = spatial.distance.cosine(face_embedding, badges_faces[badge_idx])
-            faces_scores.append((dist, args["badges"][badge_idx], face_coord, ))
+            # print(
+            #     args["badges"][badge_idx],
+            #     dist,
+            #     face_embedding[0:5],
+            #     badges_faces[badge_idx][0:5],
+            # )
+            temp_faces.append((2 - dist, args["badges"][badge_idx], face_coord, ))
+
+        total = sum(x[0] for x in temp_faces)
+        for inv_dist, badge, face_coord in temp_faces:
+            faces_scores.append((1 - (inv_dist / total), badge, face_coord, ))
 
     faces_scores.sort(key=lambda x: x[0])
+
+    # for score, badge, face_coord in faces_scores:
+    #     pos = [int(n) for n in face_coord]
+    #     print(score, badge, pos)
 
     draw_badge = set()
     draw_area = set()
     for score, badge, face_coord in faces_scores:
-        (x, y, w, h, ) = face_coord
-        face_coord = (x, y, w, h, )
+        (x, y, xx, yy) = [int(n) for n in face_coord]
+        face_coord = (x, y, xx, yy, )
 
         if badge in draw_badge or face_coord in draw_area:
             continue
@@ -124,7 +146,7 @@ def process_one_frame(face_cascade, model, frame, args, badges_faces):
 def most_likely_face(frames_window, face_idx):
     scores = defaultdict(float)
     for frame in frames_window:
-        scores[frame[face_idx][1]] += 2 - frame[face_idx][0]
+        scores[frame[face_idx][1]] += 1 - frame[face_idx][0]
     mx = -99999999999999.9
     badge = ""
     for b, c in scores.items():
@@ -132,6 +154,118 @@ def most_likely_face(frames_window, face_idx):
             mx = c
             badge = b
     return badge
+
+def detect_in_image(badges_faces, face_cascade, model, args):
+    print("[INFO] Processing image...")
+
+    image = cv2.imread(args["image"])
+    faces = process_one_frame(face_cascade, model, image, args, badges_faces)
+    for _, badge, face_coord in faces:
+        (x, y, xx, yy, ) = [int(n) for n in face_coord]
+        image = cv2.rectangle(image, (x, y, ), (xx, yy, ), (0, 255, 0, ), 2)
+        image = cv2.putText(
+            image,
+            badge,
+            (x, yy+15, ),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 200, 200),
+            4,
+            cv2.LINE_AA,
+        )
+        image = cv2.putText(
+            image,
+            badge,
+            (x, yy+15, ),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 0),
+            2,
+            cv2.LINE_AA,
+        )
+    cv2.imwrite(args["image"] + ".out.png", image)
+
+def detect_in_video(badges_faces, face_cascade, model, args):
+    FRAME_RATE = 60
+    cap_in = cv2.VideoCapture(args["video"])
+    frame_width = int(cap_in.get(3))
+    frame_height = int(cap_in.get(4))
+    cap_out = cv2.VideoWriter(args["video"] + ".out.avi", cv2.VideoWriter_fourcc('M','J','P','G'), FRAME_RATE, (frame_width,frame_height))
+
+    print("[INFO] Processing frames...")
+
+    WINDOW_SIZE = 3 * FRAME_RATE
+    _, frame = cap_in.read()
+    frames_window = [frame, ]
+    frames_window_faces = [process_one_frame(face_cascade, model, frame, args, badges_faces), ]
+    # max_frames = FRAME_RATE * 3
+
+    print("[INFO] preloading frames...")
+    with tqdm(total=WINDOW_SIZE - 1) as pbar:
+        for _ in range(WINDOW_SIZE - 1):
+            ret, frame = cap_in.read()
+
+            if not ret:
+                break
+
+            frames_window.append(frame)
+            frames_window_faces.append(list(reorder_closest_points(
+                frames_window_faces[-1],
+                list(process_one_frame(face_cascade, model, frame, args, badges_faces))
+            )))
+            pbar.update(1)
+
+    frames_counts = int(cap_in. get(cv2.CAP_PROP_FRAME_COUNT))
+    print("[INFO] processing windowed frames...")
+    with tqdm(total=frames_counts) as pbar:
+        while frames_window_faces:
+            ret, new_frame = cap_in.read()
+            if ret:
+                frames_window.append(new_frame)
+                frames_window_faces.append(list(reorder_closest_points(
+                    frames_window_faces[-1],
+                    list(process_one_frame(face_cascade, model, new_frame, args, badges_faces))
+                )))
+
+            curr_frame = frames_window.pop(0)
+            curr_frame_faces = frames_window_faces.pop(0)
+            for face_idx, face_data in enumerate(curr_frame_faces):
+                _, _, face_coord = face_data
+                (x, y, xx, yy, ) = [int(n) for n in face_coord]
+                badge = most_likely_face(frames_window_faces, face_idx)
+                curr_frame = cv2.rectangle(curr_frame, (x, y, ), (xx, yy, ), (0, 255, 0, ), 2)
+                curr_frame = cv2.putText(
+                    curr_frame,
+                    badge,
+                    (x, yy+15, ),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 200, 200),
+                    4,
+                    cv2.LINE_AA,
+                )
+                curr_frame = cv2.putText(
+                    curr_frame,
+                    badge,
+                    (x, yy+15, ),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 0, 0),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+            cap_out.write(curr_frame)
+            pbar.update(1)
+
+            # max_frames -= 1
+            # if max_frames < 1:
+            #     break
+
+    print(" done!")
+    cap_in.release()
+    cap_out.release()
+    cv2.destroyAllWindows()
 
 def main():
     args = get_args()
@@ -142,7 +276,11 @@ def main():
     model.eval()
 
     print("[INFO] loading face model...")
-    face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
+    face_cascade = MTCNN(
+        margin=0, post_process=False, select_largest=False,
+        selection_method="probability", keep_all=True,
+        device=torch.device('cpu')
+    )
 
     print("[INFO] detecting faces in badges...")
     badges_faces = [
@@ -150,81 +288,10 @@ def main():
         for i in args["badges"]
     ]
 
-    FRAME_RATE = 19
-    cap_in = cv2.VideoCapture(args["video"])
-    frame_width = int(cap_in.get(3))
-    frame_height = int(cap_in.get(4))
-    cap_out = cv2.VideoWriter(args["video"] + "out.avi", cv2.VideoWriter_fourcc('M','J','P','G'), FRAME_RATE, (frame_width,frame_height))
+    if args["video"]:
+        detect_in_video(badges_faces, face_cascade, model, args)
 
-    print("[INFO] Processing frames...")
-
-    WINDOW_SIZE = 3 * FRAME_RATE
-    _, frame = cap_in.read()
-    frames_window = [frame, ]
-    frames_window_faces = [process_one_frame(face_cascade, model, frame, args, badges_faces), ]
-    max_frames = FRAME_RATE * 90
-    for _ in range(WINDOW_SIZE - 1):
-        ret, frame = cap_in.read()
-
-        if not ret:
-            break
-
-        sys.stdout.write("|")
-        sys.stdout.flush()
-
-        frames_window_faces.append(list(reorder_closest_points(
-            frames_window_faces[-1],
-            list(process_one_frame(face_cascade, model, frame, args, badges_faces))
-        )))
-
-    while frames_window_faces:
-        sys.stdout.write("+")
-        sys.stdout.flush()
-
-        ret, new_frame = cap_in.read()
-        if ret:
-            frames_window.append(new_frame)
-            frames_window_faces.append(list(reorder_closest_points(
-                frames_window_faces[-1],
-                list(process_one_frame(face_cascade, model, new_frame, args, badges_faces))
-            )))
-
-        curr_frame = frames_window.pop(0)
-        curr_frame_faces = frames_window_faces.pop(0)
-        for face_idx, face_data in enumerate(curr_frame_faces):
-            _, _, face_coord = face_data
-            (x, y, _, h, ) = face_coord
-            badge = most_likely_face(frames_window_faces, face_idx)
-            curr_frame = cv2.putText(
-                curr_frame,
-                badge,
-                (x, y+h+15, ),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 200, 200),
-                4,
-                cv2.LINE_AA,
-            )
-            curr_frame = cv2.putText(
-                curr_frame,
-                badge,
-                (x, y+h+15, ),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 0, 0),
-                2,
-                cv2.LINE_AA,
-            )
-
-        cap_out.write(curr_frame)
-
-        max_frames -= 1
-        if max_frames < 1:
-            break
-
-    print(" done!")
-    cap_in.release()
-    cap_out.release()
-    cv2.destroyAllWindows()
+    if args["image"]:
+        detect_in_image(badges_faces, face_cascade, model, args)
 
 main()
